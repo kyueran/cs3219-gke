1f6995ce3e432880a8f2b0076620d523d6d9a8b4
Kang Yue Ran <kangyueran09@gmail.com>
2023-11-11 00:56:29 +0800

    update cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,7 @@
 kubectl delete ingress --all
 kubectl delete deployment --all
 kubectl delete service --all
+kubectl delete pvc --all
 kubectl delete configmap --all
 kubectl delete secret --all
 kubectl delete pod --all
